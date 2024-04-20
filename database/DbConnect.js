1c08bef85a19b5d1d527536364414860d2a8fc03
shankar kumar <shankarkum20@gmail.com>
2024-04-20 11:15:10 +0530

    first commit

--- database/DbConnect.js
+++ database/DbConnect.js
@@ -5,7 +5,7 @@ dotenv.config()
 
 const dbconect=async()=>{
     try{
-        await mongoose.connect(process.env.MONGO_URL)
+        await mongoose.connect(`${process.env.MONGO_URL}`)
         console.log( `connected to ${mongoose.connection.host}`)
     }
 catch(error){

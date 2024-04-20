caa0e5a226fa229584d50c50f53e75a45c891d76
shankar kumar <shankarkum20@gmail.com>
2024-04-20 13:46:02 +0530

    first commit

--- database/DbConnect.js
+++ database/DbConnect.js
@@ -10,7 +10,10 @@ const MONGO_URL =process.env.MONGO_URL
 
 const dbconect=async()=>{
     try{
-        await mongoose.connect(`${MONGO_URL}`)
+        await mongoose.connect(`${MONGO_URL}`,{
+            useNewUrlParser: true,
+        useUnifiedTopology: true
+        })
         console.log( `connected to ${mongoose.connection.host}`)
     }
 catch(error){
